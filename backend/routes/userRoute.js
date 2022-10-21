44c445a6205d9df6050a4f8cc1f9b0d5783df323
Harsh Kanjiya <harshkanjiya001@gmail.com>
2022-10-21 13:35:14 +0530

    .

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser, logOut } = require('../controllers/userController');
+const { registerUser, loginUser, logOut, forgotPassword } = require('../controllers/userController');
 const router = express.Router();
 
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+router.route("password/forgot").post(forgotPassword)
 router.route("/logout").get(logOut)
 
 
