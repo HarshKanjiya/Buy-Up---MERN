7491ba02761abe51cfe34c3254cf336abfa3b87f
Harsh Kanjiya <harshkanjiya001@gmail.com>
2022-11-01 18:20:14 +0530

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import webfont from "webfontloader";
+import { useEffect } from "react";
+import Header from "./components/layout/Header/Header";
+import Footer from "./components/layout/Footer/Footer";
+import Home from "./components/Home/Home";
 
 function App() {
+  useEffect(() => {
+    webfont.load({
+      google: {
+        families: ["Poppins", "Roboto"],
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-       <p>starting</p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+      <Route exact path="/" element={ <Home/> } />
+</Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
