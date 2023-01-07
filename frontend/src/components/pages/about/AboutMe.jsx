2c8b11be3dd3d03d5ac9dd2f0e01d63de7ecece1
Harsh Kanjiya <harshkanjiya001@gmail.com>
2023-01-08 01:42:18 +0530

    `

--- frontend/src/components/pages/about/AboutMe.jsx
+++ frontend/src/components/pages/about/AboutMe.jsx
@@ -1,30 +1,66 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Insta from "../../../assets/images/instagram.png";
 import Linkdin from "../../../assets/images/linkedin.png";
 import Github from "../../../assets/images/github.png";
 import Gmail from "../../../assets/images/gmail.png";
 
 const AboutMe = () => {
+  const navigate = useNavigate();
   return (
-    <Wrapper>
-      <Cont>
-        <Header>
+    <Wrapper
+      key="about me"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{
+        duration: 0.4,
+        ease: "easeInOut",
+        type: "tween",
+      }}
+    >
+      <Cont
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.4, ease: "easeOut", delay: 0.2 }}
+      >
+        <Header
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.4, ease: "easeOut", delay: 0.5 }}
+        >
+          <BackBtn
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            <ArrowBackIcon fontSize="small" />
+          </BackBtn>
           <p>
             About <span>ME</span>
           </p>
         </Header>
         <Body>
           <Left>
-            <p>Hi there 🙋‍♂️ </p>
-            <p>
-              I'm <span>Harsh Kanjiya</span>,
-            </p>
-            <p>
-              a self taught <span>Full stack developer</span> &{" "}
-              <span>UIUX Designer</span>.
-            </p>
+            <motion.div
+              initial={{ y: 30, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.4, ease: "easeOut", delay: 0.8 }}
+            >
+              <p>Hi there 🙋‍♂️ </p>
+              <p>
+                I'm <span>Harsh Kanjiya</span>,
+              </p>
+              <p>
+                a self taught <span>Full stack developer</span> &{" "}
+                <span>UIUX Designer</span>.
+              </p>
+            </motion.div>
             <br />
             <p>
               this is my first Full stack project after learning for 5 months
@@ -71,6 +107,22 @@ const AboutMe = () => {
 };
 
 export default AboutMe;
+
+const BackBtn = styled.div`
+  background-color: white;
+  height: 2.5rem;
+  width: 2.5rem;
+  border-radius: 2rem;
+  display: grid;
+  place-content: center;
+  margin: 0 1rem;
+  box-shadow: 0 0 1.5rem rgba(0, 0, 0, 0.06);
+  transition: 300ms;
+  &:hover {
+    box-shadow: 0 0 1.7rem rgba(0, 0, 0, 0.16);
+    transform: rotate(360deg);
+  }
+`;
 
 const Links = styled.div`
   display: flex;
@@ -119,9 +171,11 @@ const Body = styled.div`
     margin: 0.5rem;
   }
 `;
-const Header = styled.div`
+const Header = styled(motion.div)`
   margin: 1rem;
   border-bottom: 1px solid #d1d1d1;
+  display: flex;
+  gap: 1rem;
   p {
     font-size: 2rem;
     font-weight: 900;
@@ -131,7 +185,7 @@ const Header = styled.div`
     color: #232323;
   }
 `;
-const Cont = styled.div`
+const Cont = styled(motion.div)`
   display: flex;
   flex-direction: column;
   background-color: white;
@@ -143,12 +197,12 @@ const Cont = styled.div`
   box-shadow: 8px 8px 1rem rgba(0, 0, 0, 0.2),
     -8px -8px 1rem rgba(255, 255, 255, 0.1);
 
-    @media (max-width: 600px) {
+  @media (max-width: 600px) {
     padding: 0.5em;
     margin: 0.5rem;
   }
 `;
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   padding: 1rem;
   width: 100%;
   display: flex;
